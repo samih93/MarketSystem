128f0a88cbca78eea14040fe2d29aa7d7e12df3a
samih damaj <samih_damaj@hotmail.com>
2022-03-27 20:46:32 +0300

    add status message

--- lib/screens/add_product/add_product_controller.dart
+++ lib/screens/add_product/add_product_controller.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 import 'package:marketsystem/models/product.dart';
 import 'package:marketsystem/shared/local/marketdb_helper.dart';
+import 'package:marketsystem/shared/toast_message.dart';
 
 class AddProductController extends GetxController {
   MarketDbHelper marketdb = MarketDbHelper.db;
@@ -14,17 +15,23 @@ class AddProductController extends GetxController {
 
   //NOTE insert Product
 
-  //  add event by model
+  var statusMessage = "".obs;
+  var statusInsertMessage = ToastStatus.Error.obs;
+  //  add evenstatusInsertMessaget by model
   Future<void> insertProductByModel({required ProductModel model}) async {
     var dbm = await marketdb.database;
-
-    await dbm.insert("products", model.toJson());
-    // await marketdb.database
-    //     .rawQuery("select * from products where id='${id}'")
-    //     .then((value) {
-    //   value.forEach((element) {
-    //     // update();
-    //   });
-    // });
+    await marketdb.database
+        .rawQuery("select * from products where barcode='${model.barcode}'")
+        .then((value) async {
+      if (value.length > 0) {
+        statusMessage.value = "product Alreay Exist try ";
+        statusInsertMessage.value = ToastStatus.Error;
+      } else {
+        await dbm.insert("products", model.toJson());
+        statusMessage.value = "product inserted successfully";
+        statusInsertMessage.value = ToastStatus.Success;
+      }
+      update();
+    });
   }
 }
