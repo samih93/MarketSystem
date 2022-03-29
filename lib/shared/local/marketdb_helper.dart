d5ee9f1f7a0c4568a5666a2f081d792c72957997
samih damaj <samih_damaj@hotmail.com>
2022-03-29 21:27:46 +0300

    add table store

--- lib/shared/local/marketdb_helper.dart
+++ lib/shared/local/marketdb_helper.dart
@@ -11,11 +11,20 @@ class MarketDbHelper {
       version: 1,
       onCreate: (db, version) {
         print("database created");
+        // NOTE create table product
         db
             .execute(
                 "Create Table products(barcode TEXT ,name TEXT,price INTEGER)")
             .then((value) => print('products table created'))
             .catchError((onError) => print(onError.toString()));
+
+            //NOTE create table store
+            db
+            .execute(
+                "Create Table store(barcode TEXT ,name TEXT,price INTEGER,qty INTEGER)")
+            .then((value) => print('products store created'))
+            .catchError((onError) => print(onError.toString()));
+
       },
       onOpen: (database) {
         print('database opened');
