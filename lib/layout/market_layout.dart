d2601ef87415be5005fd317a24ddc502a21dc640
isfacademy <isfacademy.2023@gmail.com>
2022-04-04 13:23:02 +0300

    enhacement

--- lib/layout/market_layout.dart
+++ lib/layout/market_layout.dart
@@ -1,53 +1,52 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:marketsystem/layout/market_controller.dart';
+import 'package:marketsystem/providers/layout_provider.dart';
 import 'package:marketsystem/shared/components/default_text_form.dart';
 import 'package:marketsystem/shared/constant.dart';
+import 'package:provider/provider.dart';
 
 class MarketLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<MarketController>(
-        init: Get.find<MarketController>(),
-        builder: (controller) => Scaffold(
-              appBar: AppBar(
-                title: controller.issearchingInProducts
-                    ? _buildSearchField(
-                        controller,
-                        'search in products...',
-                      )
-                    : Text(
-                        controller.appbar_title[controller.currentIndex]
-                            .toString(),
-                      ),
-                actions: controller.issearchingInProducts == false
-                    ? [
-                        IconButton(
-                            onPressed: () {
-                              controller.onChangeSearchInProductsStatus(true);
-                            },
-                            icon: Icon(Icons.search))
-                      ]
-                    : [],
-              ),
-              body: controller.screens[controller.currentIndex],
-              bottomNavigationBar: BottomNavigationBar(
-                elevation: 30,
-                type: BottomNavigationBarType.fixed,
-                selectedItemColor: defaultColor,
-                onTap: (index) {
-                  print(index);
+    return Consumer<LayoutController>(builder: (context, controller, child) {
+      return Scaffold(
+        appBar: AppBar(
+          title: controller.issearchingInProducts
+              ? _buildSearchField(
+                  'search in products...',
+                )
+              : Text(
+                  controller.appbar_title[controller.currentIndex].toString(),
+                ),
+          actions: controller.issearchingInProducts == false
+              ? [
+                  IconButton(
+                      onPressed: () {
+                        controller.onChangeSearchInProductsStatus(true);
+                      },
+                      icon: Icon(Icons.search))
+                ]
+              : [],
+        ),
+        body: controller.screens[controller.currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          elevation: 30,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: defaultColor,
+          onTap: (index) {
+            print(index);
 
-                  controller.onchangeIndex(index);
-                },
-                currentIndex: controller.currentIndex,
-                items: controller.bottomItems,
-              ),
-            ));
+            controller.onchangeIndex(index);
+          },
+          currentIndex: controller.currentIndex,
+          items: controller.bottomItems,
+        ),
+      );
+    });
   }
 
   _buildSearchField(
-    MarketController c,
     String hint,
   ) {
     return Container(
@@ -57,7 +56,7 @@ class MarketLayout extends StatelessWidget {
           focus: true,
           onchange: (value) {
             if (value!.length > 1) {
-              c.search_In_Products(value);
+              //c.search_In_Products(value);
             }
           },
           inputtype: TextInputType.name,
@@ -72,7 +71,7 @@ class MarketLayout extends StatelessWidget {
               color: Colors.white,
             ),
             onPressed: () {
-              c.clearSearch();
+              //  c.clearSearch();
             },
           )),
     );
