40a094f4b87f1b065dd70d8c09214edf851099da
samih damaj <samih_damaj@hotmail.com>
2022-04-24 12:50:26 +0300

    add animation in splash

--- lib/screens/splash_screen/splash_screen.dart
+++ lib/screens/splash_screen/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:get/get.dart';
 import 'package:marketsystem/controllers/products_controller.dart';
 import 'package:marketsystem/layout/market_layout.dart';
@@ -24,24 +25,25 @@ class _SplashScreenState extends State<SplashScreen> {
         print('getting products');
       });
     });
-
-    super.initState();
   }
 
   Future _load_products() async {
-    await MarketDbHelper.db.init().then((value) async {
-      await getDatabasesPath().then((value) async {
-        print(value + "/Market.db");
-        databasepath = value + "/Market.db";
-        await Provider.of<ProductsController>(context, listen: false)
-            .getAllProduct()
-            .then((value) => Get.off(MarketLayout()));
+    Future.delayed(Duration(seconds: 2)).then((value) async {
+      await MarketDbHelper.db.init().then((value) async {
+        await getDatabasesPath().then((value) async {
+          print(value + "/Market.db");
+          databasepath = value + "/Market.db";
+          await Provider.of<ProductsController>(context, listen: false)
+              .getAllProduct()
+              .then((value) => Get.off(MarketLayout()));
+        });
       });
     });
+  }
 
-    // Future.delayed(Duration(seconds: 1)).then((value) async {
-
-    // });
+  @override
+  void dispose() {
+    super.dispose();
   }
 
   @override
@@ -61,11 +63,19 @@ class _SplashScreenState extends State<SplashScreen> {
                       image: AssetImage("assets/splash_screen.png")),
                 ),
                 SizedBox(
-                  height: 5,
+                  height: 10,
                 ),
                 Text(
                   "Market System",
-                  style: TextStyle(color: Colors.white, letterSpacing: 1),
+                  style: TextStyle(
+                      color: Colors.white, letterSpacing: 1, fontSize: 30),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                SpinKitWave(
+                  color: Colors.white,
+                  size: 35.0,
                 ),
               ],
             )),
