bea0cde078530545f18b37fb500c4687012a3372
isfacademy <isfacademy.2023@gmail.com>
2022-04-26 12:36:25 +0300

    add more chart

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:marketsystem/shared/constant.dart';
+import 'package:marketsystem/shared/styles.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 import 'package:syncfusion_flutter_charts/sparkcharts.dart';
 
@@ -27,30 +29,141 @@ class DashBoardScreen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(
           title: const Text('DashBoard'),
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: myLinearGradient,
+            ),
+          ),
         ),
-        body: Column(children: [
-          //Initialize the chart widget
-          SfCartesianChart(
-              enableSideBySideSeriesPlacement: false,
-              primaryXAxis: CategoryAxis(),
-              // Chart title
-              title: ChartTitle(text: 'Daily Sales'),
-              // disable legend
-              legend: Legend(isVisible: false),
-              // Enable tooltip
-              tooltipBehavior: TooltipBehavior(enable: true),
-              series: <ChartSeries<_SalesData, String>>[
-                LineSeries<_SalesData, String>(
-                    dataSource: data,
-                    xValueMapper: (_SalesData sales, _) => sales.year,
-                    yValueMapper: (_SalesData sales, _) => sales.sales,
-                    name: 'Sales',
-                    // Enable data label
-                    dataLabelSettings: DataLabelSettings(
-                      isVisible: true,
-                    ))
-              ]),
-        ]));
+        body: SingleChildScrollView(
+          child: Column(children: [
+            SizedBox(
+              height: 20,
+            ),
+            //Initialize the chart widget
+            Container(
+              color: Colors.amberAccent,
+              height: 30,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      "Daily Sales",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        letterSpacing: 1,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SfCartesianChart(
+                enableSideBySideSeriesPlacement: false,
+                primaryXAxis: CategoryAxis(),
+                // Chart title
+                // title: ChartTitle(
+                //   text: 'Daily Sales',
+                // ),
+                // disable legend
+                legend: Legend(isVisible: false),
+                // Enable tooltip
+                tooltipBehavior: TooltipBehavior(enable: true),
+                series: <ChartSeries<_SalesData, String>>[
+                  LineSeries<_SalesData, String>(
+                      dataSource: data,
+                      xValueMapper: (_SalesData sales, _) => sales.year,
+                      yValueMapper: (_SalesData sales, _) => sales.sales,
+                      name: 'Sales',
+                      // Enable data label
+                      dataLabelSettings: DataLabelSettings(
+                        isVisible: true,
+                      ))
+                ]),
+            Container(
+              color: Colors.amberAccent,
+              height: 30,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      "Best Selling",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        letterSpacing: 1,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SfCartesianChart(
+              plotAreaBorderWidth: 0,
+              // title: ChartTitle(text: 'Best Selling'),
+              primaryXAxis: CategoryAxis(
+                  majorGridLines: const MajorGridLines(width: 0),
+                  labelIntersectAction: AxisLabelIntersectAction.rotate45),
+              primaryYAxis: NumericAxis(
+                  axisLine: const AxisLine(width: 0),
+                  labelFormat: '{value}%',
+                  majorTickLines: const MajorTickLines(size: 0)),
+              series: _getDefaultColumnSeries(),
+              tooltipBehavior: TooltipBehavior(
+                  enable: true, header: '', canShowMarker: false),
+            ),
+            Container(
+              color: Colors.amberAccent,
+              height: 30,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Text(
+                      "Most Profitable",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        letterSpacing: 1,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SfCartesianChart(
+              plotAreaBorderWidth: 0,
+              // title: ChartTitle(text: 'Best Selling'),
+              primaryXAxis: CategoryAxis(
+                  majorGridLines: const MajorGridLines(width: 0),
+                  labelIntersectAction: AxisLabelIntersectAction.rotate45),
+              primaryYAxis: NumericAxis(
+                  axisLine: const AxisLine(width: 0),
+                  labelFormat: '{value} LL',
+                  majorTickLines: const MajorTickLines(size: 0)),
+              series: _getDefaultColumnSeries(),
+              tooltipBehavior: TooltipBehavior(
+                  enable: true, header: '', canShowMarker: false),
+            ),
+          ]),
+        ));
+  }
+
+  /// Get default column series
+  List<ColumnSeries<ChartSampleData, String>> _getDefaultColumnSeries() {
+    return <ColumnSeries<ChartSampleData, String>>[
+      ColumnSeries<ChartSampleData, String>(
+        dataSource: <ChartSampleData>[
+          ChartSampleData(x: 'China', y: 0.541),
+          ChartSampleData(x: 'Brazil', y: 0.818),
+          ChartSampleData(x: 'Bolivia', y: 1.51),
+          ChartSampleData(x: 'Mexico', y: 1.302),
+          ChartSampleData(x: 'Egypt', y: 2.017),
+          ChartSampleData(x: 'Mongolia', y: 1.683),
+        ],
+        xValueMapper: (ChartSampleData sales, _) => sales.x as String,
+        yValueMapper: (ChartSampleData sales, _) => sales.y,
+        dataLabelSettings: const DataLabelSettings(
+            isVisible: true, textStyle: TextStyle(fontSize: 10)),
+      )
+    ];
   }
 }
 
@@ -59,4 +172,10 @@ class _SalesData {
 
   final String year;
   final double sales;
+}
+
+class ChartSampleData {
+  ChartSampleData({required this.x, required this.y});
+  String x;
+  double y;
 }
