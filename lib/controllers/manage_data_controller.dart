f5d23fcc414002f0ec7ccd28d20b9fe44005bd7e
samih damaj <samih_damaj@hotmail.com>
2022-05-10 10:58:25 +0300

    add managecontroller and firestorage

--- lib/controllers/manage_data_controller.dart
+++ lib/controllers/manage_data_controller.dart
@@ -1,3 +1,22 @@
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 
-class ManageDataController extends ChangeNotifier {}
+class ManageDataController extends ChangeNotifier {
+  Future<void> uploadDatabase(String token, File profile_image) async {
+    FirebaseStorage.instance
+        .ref('')
+        .child('users/$token')
+        .putFile(profile_image)
+        .then((value) {
+      value.ref.getDownloadURL().then((value) {}).catchError((error) {
+        {
+          print(error.toString());
+        }
+      });
+    }).catchError((error) {
+      print(error.toString());
+    });
+  }
+}
